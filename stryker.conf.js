325c656c0cf8c8284bae48039a8f0568e081a7fb
Daniel Stokes <d.stokes@f5.com>
2020-03-05 14:05:05 -0800

    Fix lint errors in stryker.conf.js

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,11 +1,13 @@
-module.exports = function(config) {
-  config.set({
-    mutator: "javascript",
-    packageManager: "npm",
-    reporters: ["clear-text", "progress"],
-    testRunner: "mocha",
-    transpilers: [],
-    testFramework: "mocha",
-    coverageAnalysis: "perTest"
-  });
+'use strict';
+
+module.exports = (config) => {
+    config.set({
+        mutator: 'javascript',
+        packageManager: 'npm',
+        reporters: ['clear-text', 'progress'],
+        testRunner: 'mocha',
+        transpilers: [],
+        testFramework: 'mocha',
+        coverageAnalysis: 'perTest'
+    });
 };
